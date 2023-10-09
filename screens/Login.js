3f779896177e6f2e9c5df8ffd76b783eefe2678f
Samuel Martins <samuel.am709@gmail.com>
2023-10-09 01:42:51 -0300

    close #2

--- screens/Login.js
+++ screens/Login.js
@@ -1,16 +1,29 @@
+import React from 'react';
 import { View, Text, TextInput, Button, StyleSheet, Image } from 'react-native';
 
 const Login = () => {
   return (
     <View style={estilos.container}>
-      <Image
-        style={estilos.logo}
-        source={require('../assets/snack-icon.png')}
-      />
+      <Text style={estilos.topo}>My Tasks</Text>
+      <Image style={estilos.logo} source={require('../assets/perfil.png')} />
       <Text style={estilos.titulo}>Login</Text>
-      <TextInput style={estilos.input} placeholder={'E-mail'} keyboardType={'email-address'} />
-      <TextInput style={estilos.input} placeholder={'Senha'} secureTextEntry={true} />
+      <TextInput
+        style={estilos.input}
+        placeholder={'E-mail'}
+        keyboardType={'email-address'}
+      />
+      <TextInput
+        style={estilos.input}
+        placeholder={'Senha'}
+        secureTextEntry={true}
+      />
       <Button title="entrar" />
+      <View style={{ marginVertical: 10 }}>
+        {/* Espaço vertical entre os botões */}
+      </View>
+
+      <Button title="Não tem conta? Cadastrar" />
+      <Text style={estilos.esqueceuSenha}> Esqueceu sua senha? </Text>
     </View>
   );
 };
@@ -18,17 +31,19 @@ const Login = () => {
 const estilos = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     paddingHorizontal: 16,
+    marginTop: 20,
   },
   logo: {
     height: 72,
     width: 72,
-    alignSelf: 'center'
+    alignSelf: 'center',
+    marginTop: 40,
   },
   titulo: {
     fontSize: 28,
-    fontWeight: 400,
+    fontWeight: 'bold',
     textAlign: 'center',
     marginBottom: 24,
   },
@@ -37,6 +52,19 @@ const estilos = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomColor: 'black',
     marginBottom: 16,
+  },
+  topo: {
+    alignSelf: 'center',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 24,
+    fontFamily: '',
+  },
+  esqueceuSenha: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 24,
   },
 });
 
